5855eccf167afbe1d24c07e7d0096aaa78764c0b
NextSap <nextsap@outlook.com>
2023-07-20 17:54:26 +0200

    update manager

--- src/main/java/com/nextsap/manager/InteractiveShell.java
+++ src/main/java/com/nextsap/manager/InteractiveShell.java
@@ -22,7 +22,7 @@ public class InteractiveShell {
         InputReaderUtil inputReaderUtil = InputReaderUtil.getInstance();
         boolean exit = false;
 
-        util.print("${blue}Bienvenue dans le manager de l'application${reset}");
+        util.print("${blue}Bienvenue dans le manager de l'application ${red}PAY MY BUDDY{blue} (P7)${reset}");
 
         while (!exit) {
             util.print("${yellow}INFO: Veuillez saisir une option${reset} (Tapez 1 pour afficher les options disponibles)");
@@ -69,18 +69,17 @@ public class InteractiveShell {
 
     private void command2(String[] args, boolean useWindows) {
         String error = """
-                    ${red}ERREUR: Les arguments entrés sont invalides${reset}
-                    ${yellow}INFO: Secrets à initialiser :
-                       - jwt.secret                     ${red}Requis${yellow}
-                       - spring.datasource.username     ${red}Requis${yellow}
-                       - spring.datasource.password     ${red}Requis${yellow}
-                       
-                    ${yellow}INFO: Exemple d'utilisation:
-                       2 jwt.secret=secret spring.datasource.username=root spring.datasource.password=root
-                                        
-                    ${green}Si vous souhaitez la facilité (ce que je peux comprendre), vous pouvez utiliser les arguments définis par défaut.
-                    ${green}Pour cela, veuillez saisir la commande suivante:
-                       2 ignore
+                    ${reset}| ${red}ERREUR: Les arguments entrés sont invalides${reset}
+                    ${reset}| ${yellow}INFO: Secrets à initialiser :
+                    ${reset}|    - jwt.secret                     ${red}Requis${yellow}
+                    ${reset}|    - spring.datasource.username     ${red}Requis${yellow}
+                    ${reset}|    - spring.datasource.password     ${red}Requis${yellow}
+                    ${reset}|    
+                    ${reset}| ${yellow}INFO: Exemple d'utilisation:
+                    ${reset}|    ${yellow}2 jwt.secret=secret spring.datasource.username=root spring.datasource.password=root
+                    ${reset}|                     
+                    ${reset}| ${yellow}INFO: Exemple d'utilisation pour lancer en mode ⭐️${purple}flemmard ${yellow}:
+                    ${reset}|    ${yellow}2 ignore
                     """;
         if (args.length < 4 && args.length != 2) {
             util.print(error);
@@ -95,7 +94,7 @@ public class InteractiveShell {
         }
 
         if (args.length == 2 && args[1].equals("ignore")) {
-           util.exec("docker-compose up -d --build && docker-compose logs -f | grep -m 1 'Development mode should NOT be used in production installations!' && docker exec paymybuddy-vault vault login 00000000-0000-0000-0000-000000000000 && sleep .5 && docker exec paymybuddy-vault vault kv put secret/paymybuddy jwt.secret=secret spring.datasource.username=root spring.datasource.password=root", true, 0);
+           util.exec("docker-compose up -d --build  " + operator + " docker-compose logs -f | grep -m 1 'Development mode should NOT be used in production installations!' " + operator + " docker exec paymybuddy-vault vault login 00000000-0000-0000-0000-000000000000 " + operator + " "+sleepCommand+" .5 " + operator + " docker exec paymybuddy-vault vault kv put secret/paymybuddy jwt.secret=secret spring.datasource.username=root spring.datasource.password=root", true, 0);
            return;
         }
 
@@ -104,7 +103,7 @@ public class InteractiveShell {
             return;
         }
 
-        util.exec("docker-compose up -d --build " + operator + " " + sleepCommand + " 5 " + operator + " docker exec paymybuddy-vault vault login 00000000-0000-0000-0000-000000000000 " + operator + " " + sleepCommand + " 2 " + operator + " docker exec paymybuddy-vault vault kv put secret/paymybuddy " + args[0] + " " + args[1] + " " + args[2], true, 0);
+        util.exec("docker-compose up -d --build " + operator + " " + sleepCommand + " 5 " + operator + " docker exec paymybuddy-vault vault login 00000000-0000-0000-0000-000000000000 " + operator + " " + sleepCommand + " 2 " + operator + " docker exec paymybuddy-vault vault kv put secret/paymybuddy " + args[1] + " " + args[2] + " " + args[3], true, 0);
     }
 
     private void command3() {
